8984abbf63a61e58c4ad3ec6ac6891eb288b5991
Baklanova Veronika <dexp51477@gmail.com>
2026-03-11 10:35:19 +0300

    feat: add spring app init

---
+++ backend/src/main/java/com/scooter/violation/processing_service/ScooterViolationMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.scooter.violation.processing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScooterViolationMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScooterViolationMonitorApplication.class, args);
+	}
+
+}
