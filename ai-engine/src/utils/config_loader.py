1596304f13e67d697ecbc1aa4649a9fff08b3f7d
Baklanova Veronika <dexp51477@gmail.com>
2026-03-17 19:13:05 +0300

    fix: fix base config

--- ai-engine/src/utils/config_loader.py
+++ ai-engine/src/utils/config_loader.py
@@ -6,13 +6,21 @@ def load_config(config_path: str) -> dict:
     """Загружает конфигурацию из YAML файла"""
     if not os.path.exists(config_path):
         base_config = {
-            'paths': {
-                'detection_model': 'models/detection/best.pt',
-            },
             'models': {
                 'detection': {
-                    'confidence_threshold': 0.6,
-                    'iou_threshold': 0.7
+                        'model_path': 'models/detection/best.pt',
+                        'confidence_threshold': 0.44,
+                        'iou_threshold': 0.8,
+                },
+                'pose_estimation': {
+                    'model_path': 'models/pose_classification/yolo11m-pose.pt'
+                },
+                'pose_classification': {
+                    'ensemble_paths': ['models/pose_classification/pose_classifier.joblib', 'models/pose_classification/pose_classifier_B.joblib'],
+                    'ensemble_weights': [1, 1]
+                    },
+                'segmentation': {
+                    'segmentation_model': 'facebook/mask2former-swin-large-mapillary-vistas-semantic'
                 },
             },
         }
