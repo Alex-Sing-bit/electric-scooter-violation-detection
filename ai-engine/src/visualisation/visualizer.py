2dd50f8b90a107f6660d181dea55afa3c4dc3916
Baklanova Veronika <dexp51477@gmail.com>
2026-04-24 16:56:00 +0300

    feat: add violation text visualisation

--- ai-engine/src/visualisation/visualizer.py
+++ ai-engine/src/visualisation/visualizer.py
@@ -3,6 +3,63 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from segmantation.segmentation_categories import SegmentConfig
+from utils.bbox_utils import join_bboxes
+
+
+def visualize_violation(image, prediction):
+    if image is None or prediction is None:
+        return image
+
+    if  prediction['violations'] is None or len(prediction['violations']) == 0:
+        return image
+
+    x1, y1, x2, y2 = map(int, join_bboxes(prediction['bbox'], prediction['nearest_scooter']['bbox']))
+    color = (0, 0, 255)
+    cv2.rectangle(image, (x1, y1), (x2, y2), color, 5)
+    cv2.putText(image, " ".join(set(prediction['violations'])), (x1 + 5, y1 + 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 1)
+
+    return image
+
+
+def visualize_warning(image, warning):
+    if image is None or warning is None:
+        return image
+
+    if warning['warning'] is None or len(warning['warning']) == 0:
+        return image
+
+    x1, y1, x2, y2 = map(int, warning['bbox'])
+    cv2.rectangle(image, (x1, y1), (x2, y2), (0, 165, 255), 2)
+
+    return image
+
+
+def print_violations(image, violations):
+    border_size = 50
+    h, w = image.shape[:2]
+
+    bordered_image = cv2.copyMakeBorder(
+        image,
+        0, border_size,
+        0, 0,
+        cv2.BORDER_CONSTANT,
+        value=(255, 255, 255)
+    )
+
+    violations_text = " ".join(set(violations))
+    text_position = (20, h + border_size - 20)
+    cv2.putText(
+        bordered_image,
+        violations_text,
+        text_position,
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.7,
+        (0, 0, 255),
+        2
+    )
+
+    return bordered_image
 
 
 class ResultVisualizer:
@@ -14,8 +71,6 @@ class ResultVisualizer:
 
         seg_image = np.array(image.copy())
         overlay = np.zeros_like(seg_image)
-
-        segmentation_results['unknown'] = []
 
         legend_info = []
         for category in segmentation_results:
@@ -55,15 +110,25 @@ class ResultVisualizer:
 
     def visualize_predictions(self, image, predictions, save_path=None):
         """Визуализация предсказаний на изображении"""
-        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if image is None:
+            return None
 
-        # Цвета для разных классов
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        if predictions is None:
+            plt.figure(figsize=(12, 8))
+            plt.imshow(image_rgb)
+            plt.axis('off')
+            plt.show()
+
+            return image_rgb
+
         class_colors = {
-            'riding': (0, 255, 0),  # зеленый
-            'pushing': (255, 165, 0),  # оранжевый
-            'on_foot': (255, 0, 0),  # красный
-            'other': (128, 0, 128)  # фиолетовый
+            'riding': (0, 255, 0),
+            'pushing': (255, 165, 0),
+            'climbing': (255, 255, 0),
+            'on_foot': (255, 0, 0),
+            'other': (128, 0, 128)
         }
 
         for pred in predictions:
@@ -72,37 +137,30 @@ class ResultVisualizer:
             confidence = pred['confidence']
             person_id = pred['person_id']
 
-            # Цвет для текущего класса
-            color = class_colors.get(class_name, (255, 255, 0))  # желтый по умолчанию
+            color = class_colors.get(class_name, (255, 255, 0))
 
-            # Если есть bbox, рисуем его
             if bbox is not None:
                 x1, y1, x2, y2 = map(int, bbox)
                 cv2.rectangle(image_rgb, (x1, y1), (x2, y2), color, 2)
 
-                # Текст с id и результатом
-                text = f"ID {person_id}: {class_name} ({confidence:.2f})"
+                '''text = f"ID {person_id}: {class_name} ({confidence:.2f})"
                 cv2.putText(image_rgb, text, (x1, y1 - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)'''
             else:
-                # Если нет bbox, выводим текст в угол
                 text = f"ID {person_id}: {class_name} ({confidence:.2f})"
                 cv2.putText(image_rgb, text, (10, 30 + 30 * person_id),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
 
-        # Добавляем легенду в правый верхний угол
         legend_y = 30
         for class_name, color in class_colors.items():
             cv2.putText(image_rgb, f"{class_name}", (image.shape[1] - 150, legend_y),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
             legend_y += 25
 
-        # Показываем или сохраняем результат
-        if save_path:
-            cv2.imwrite(save_path, cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR))
-            print(f"Результат сохранен в {save_path}")
-        else:
-            plt.figure(figsize=(12, 8))
-            plt.imshow(image_rgb)
-            plt.axis('off')
-            plt.show()
+        '''plt.figure(figsize=(12, 8))
+        plt.imshow(image_rgb)
+        plt.axis('off')
+        plt.show()'''
+
+        return image_rgb
+
