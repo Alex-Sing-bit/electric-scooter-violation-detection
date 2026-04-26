c04ae0f106e149a25c236122626b8c9e7bcdfbaa
Baklanova Veronika <dexp51477@gmail.com>
2026-04-26 19:11:35 +0300

    feat: add find methods to violation repository

--- backend/src/main/java/com/scooter/violation/processing_service/repository/ViolationRepository.java
+++ backend/src/main/java/com/scooter/violation/processing_service/repository/ViolationRepository.java
@@ -2,9 +2,11 @@ package com.scooter.violation.processing_service.repository;
 
 import com.scooter.violation.processing_service.entity.Violation;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.UUID;
 
 public interface ViolationRepository extends JpaRepository<Violation, UUID> {
-
+    List<Violation> findByTaskId(UUID taskId);
+    List<Violation> findByFrameId(UUID frameId);
+    List<Violation> findByType(String type);
 }
